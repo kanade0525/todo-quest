4ff7b6a26d317ed9ee73d936dc4d6dc387205274
kanade0525 <a16111k@reitaku.jp>
2021-02-10 16:12:35 +0900

    Fix: overflow

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <b-container class="mb-5">
-      <b-row class="nes-field is-inline">
-        <input v-model="value" type="text" id="dark_field" class="nes-input is-dark" placeholder="what to do...">
-        <button @click="handleAddTodo" type="button" class="nes-btn is-warning">add</button>
+      <b-row class="nes-field is-inline d-flex">
+        <input v-model="value" type="text" id="dark_field" class="nes-input is-dark w-75" placeholder="what to do...">
+        <button @click="handleAddTodo" type="button" class="nes-btn is-warning w-25">add</button>
       </b-row>
 
       <!-- <b-input-group>
